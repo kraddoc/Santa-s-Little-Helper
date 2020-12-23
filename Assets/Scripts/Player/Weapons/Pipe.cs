bf5cc446d658ccd77921f6eff4d5deab60c72f23
Alex Berlin <terehovich.alexander@gmail.com>
2020-12-23 14:14:13 +0300

    Pipe Holding Behaivior

--- Assets/Scripts/Player/Weapons/Pipe.cs
+++ Assets/Scripts/Player/Weapons/Pipe.cs
@@ -1,3 +1,4 @@
+using System;
 using SantasHelper.Interfaces;
 using UnityEngine;
 
@@ -7,11 +8,22 @@ namespace SantasHelper.Player.Weapons
     {
         [SerializeField] [Range(1, 10)] private int minDamage = 1;
         [SerializeField] [Range(2, 10)] private int maxDamage = 5;
+        [SerializeField] [Range(0.01f, 1f)] private float interpolationTime = 0.5f;
         private int _currentDamage;
+        private float _holdTime;
+        private bool _isHolding;
+        
+        
+        
         
         public void Attack(IDamageable target)
         {
             target.Damage(5);
+        }
+
+        private void Hold(float holdTime)
+        {
+            _currentDamage = (int) Mathf.Lerp(minDamage, maxDamage, holdTime / interpolationTime);
         }
     }
 }
