54035e38f10aabaa7bf1e0ddcd885630c362413a
Alex Berlin <terehovich.alexander@gmail.com>
2020-12-24 16:34:40 +0300

    Billboard script for sprite rotation

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace SantasHelper
+{
+    public class Billboard : MonoBehaviour
+    {
+        [SerializeField] private Transform camera;
+        private Transform _transform;
+
+        private void Start()
+        {
+            TryGetComponent(out _transform);
+        }
+
+        private void LateUpdate()
+        {
+            _transform.forward = camera.forward;
+        }
+    }
+}
